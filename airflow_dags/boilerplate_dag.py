c92525ed60fe82a31c1839a76c3a8f2d9e513315
Marouane Skandaji <marouane.skandaji@gmail.com>
2022-10-29 16:37:07 +0200

    Setup dag task for the spark submit operator wrapper

--- airflow_dags/boilerplate_dag.py
+++ airflow_dags/boilerplate_dag.py
@@ -2,85 +2,84 @@
 Spark dbc for testing remote job execution
 """
 
-from datetime import datetime
-from email.mime import application
 import os
-from airflow import DAG
+from datetime import datetime
 
-# from utils.spark import SparkOperator
+from airflow import DAG
 from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 
+from utils.spark import SparkOperator
 
 # regsiter the environment variables defined in the airflow user profile
 # TODO: use airflow variables instead?
-run_mode = os.environ["MODE"]
-user = os.environ["USER"]
+run_mode = os.environ['MODE']
+user = os.environ['USER']
+base_path = os.environ['HOME']
+git = f'{base_path}/git-personal'
 
 default_args = {
-    "owner": "foo",
-    "depends_on_past": False,
-    "start_date": datetime(2022, 9, 1),
+    'owner': 'foo',
+    'depends_on_past': False,
+    'start_date': datetime(2022, 9, 1),
 }
 
 spark_defaults = {
-    "spark.executor.memory": "2g",
-    "spark.executor.cores": "2",
-    "spark.dynamicAllocation.enabled": "true",
-    "spark.dynamicAllocation.maxExecutors": "4",
-    "spark.shuffle.service.enabled": "true",
+    'spark.executor.memory': '2g',
+    'spark.executor.cores': '2',
+    'spark.dynamicAllocation.enabled': 'true',
+    'spark.dynamicAllocation.maxExecutors': '4',
+    'spark.shuffle.service.enabled': 'true',
 }
 
-if run_mode == "debug": # or UAT?
-    repo_path = f"/home/{user}/git/scheduler"
+if run_mode == 'debug':  # or UAT?
+    repo_path = f'{git}/scheduler'
     schedule = None
-    input_path = "store/input.csv"
-    output_path = "store/output.csv"
-    # spark_defaults["spark.executor.memory"] = "1g"
-    # spark_defaults["spark.executor.cores"] = "1"
-    # spark_defaults["spark.dynamicAllocation.maxExecutors"] = "1"
+    input_path = f'{repo_path}/store/input.csv'
+    output_path = f'{repo_path}/store/output.csv'
 
-elif run_mode == "dev":
-    repo_path = "s3://some_s3_repo_location"
+# XXX: dummy dag for testing
+elif run_mode == 'dev':
+    repo_path = 's3://some_s3_repo_location'
     schedule = None
-    input_path = "s3://foo/bar_dev"
-    output_path = "s3://foo/bar_dev"
+    input_path = 's3://foo/bar_dev'
+    output_path = 's3://foo/bar_dev'
 
-elif run_mode == "prod":
-    repo_path = "s3://some_s3_repo_location"
-    schedule = "@daily"
-    input_path = "s3://foo/bar"
-    output_path = "s3://foo/bar"
+elif run_mode == 'prod':
+    repo_path = 's3://some_s3_repo_location'
+    schedule = '@daily'
+    input_path = 's3://foo/bar'
+    output_path = 's3://foo/bar'
 
 else:
-    raise ValueError(f"Unknown run mode: {run_mode}")
+    raise ValueError(f'Unknown run mode: {run_mode}')
 
 
 # DAG definition
 with DAG(
-    "spark-boilerplate",
+    'spark-boilerplate',
     default_args=default_args,
     schedule_interval=schedule,
     catchup=False,
     doc_md=__doc__,
 ) as dag:
 
-    # boilerplate = SparkOperator(
-    #     dag=dag,
-    #     repo_path="/home/marouane-skandaji/git/scheduler",
-    #     task_id="spark-databricks",
-    #     verbose=True,
-    #     application="/home/marouane-skandaji/git/scheduler/boilerplate.py",
-    #     **spark_defaults,
-    # )
+    boilerplate = SparkOperator(
+        dag=dag,
+        repo_path=repo_path,
+        task_id='spark-submit-boilerplate-with-zip',
+        verbose=True,
+        application=f'{repo_path}/reporting/boilerplate.py',
+        **spark_defaults,
+    )
 
     boilerplate = SparkSubmitOperator(
         dag=dag,
-        task_id="spark-on-databricks",
+        task_id='spark-submit-boilerplate',
         verbose=True,
-        application=f"{repo_path}/reporting/boilerplate.py",
+        application=f'{repo_path}/reporting/boilerplate.py',
         application_args=[
             input_path,
-            output_path
+            output_path,
         ],
     )
 
--- airflow_dags/dummy_dag.py
+++ airflow_dags/dummy_dag.py
@@ -1,36 +1,34 @@
 """
 Dummy spark job to test airflow
 """
+from __future__ import annotations
 
 from datetime import datetime, timedelta
 
 from airflow import DAG
-# from airflow.operators.python_operator import PythonOperator
-from airflow.utils.dates import days_ago
 from airflow.operators.bash import BashOperator
-
-# from utils.spark import SparkOperator
 
 # pylint: disable=C0103 # Disable warning about uppercase var names
 # pylint: disable=W0104 # Disable warning about non-effect statements on tasks
 
+
 default_args = {
-    "owner": "foo",
-    "depends_on_past": False,
-    "start_date": datetime(2015, 6, 1),
-    "email": ["foo@bar.com"],
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "retries": 1,
-    "retry_delay": timedelta(minutes=5),
+    'owner': 'foo',
+    'depends_on_past': False,
+    'start_date': datetime(2015, 6, 1),
+    'email': ['foo@bar.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG("test-dag", default_args=default_args, schedule_interval=timedelta(1))
+dag = DAG('test-dag', default_args=default_args, schedule_interval=timedelta(1))
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
-t1 = BashOperator(task_id="print_date", bash_command="date", dag=dag)
+t1 = BashOperator(task_id='print_date', bash_command='date', dag=dag)
 
-t2 = BashOperator(task_id="sleep", bash_command="sleep 5", retries=3, dag=dag)
+t2 = BashOperator(task_id='sleep', bash_command='sleep 5', retries=3, dag=dag)
 
 templated_command = """
     {% for i in range(5) %}
@@ -41,9 +39,9 @@ templated_command = """
 """
 
 t3 = BashOperator(
-    task_id="templated",
+    task_id='templated',
     bash_command=templated_command,
-    params={"my_param": "Parameter I passed in"},
+    params={'my_param': 'Parameter I passed in'},
     dag=dag,
 )
 
